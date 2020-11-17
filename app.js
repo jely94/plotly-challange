9f17625365cfe7db4d11dcd0461bb797dc058940
Jeff Lyng <jely94@icloud.com>
2020-11-17 14:15:05 -0600

    added code

--- app.js
+++ app.js
@@ -34,8 +34,47 @@ d3.json("data/samples.json").then(function(data) {
 
   }
 
-  function createGraph(subject){
-        console.log(subject)
-  }
+   function createGraph(subject){
+         console.log(subject)
+         sampleData = jsonData.samples.filter(testData => testData.id == subject)[0];
+         console.log(sampleData)
+         var selector = d3.select("#bar")
+         selector.html("")
+         var sampleValues = sampleData.sample_values.slice(0,10).reverse();
+         console.log(sampleValues)
+         var idValues = (sampleData.otu_ids.slice(0, 10)).reverse();
+         console.log(idValues)
+         var idOTDValues = idValues.map(d => "OTU " + d)
+         console.log(`OTU IDS: ${idOTDValues}`)
+         var otuLabels = sampleData.otu_labels.slice(0, 10);
+         console.log(`Sample Values: ${sampleValues}`)
+         console.log(`ID Values: ${idValues}`)
 
 
+         var trace1 = {
+             x: sampleValues,
+             y: idOTDValues,
+             text: otuLabels,
+             type: "bar",
+             orientation: "h",
+         };
+
+         var data = [trace1];
+
+         var layout = {
+             title: "Top Ten OTUs",
+             yaxis:{
+                 tickmode: "linear",
+             },
+             margin: {
+                 l: 100,
+                 r: 100,
+                 t: 30,
+                 b: 20
+             }
+         };
+
+         Plotly.newPlot("bar", data, layout);
+     }
+
+   
